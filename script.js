bd820c1a97ff0dfc25419987470d9e96b8982823
Maroni Neves <maroni251096@gmail.com>
2023-06-26 23:37:10 -0300

    add send file

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+document.querySelector('#file').addEventListener('change', function(){
+  document.querySelector('.text').textContent = this.files[0].name;
+})
+
 function toggleMode() {
   const html = document.documentElement
 
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 }
